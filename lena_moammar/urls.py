e52e5b9f0f5bc545964b4318402d3b52b92d0510
ZeyadAlshaikh <alshaikh.zeyad@gmail.com>
2024-12-18 16:39:49 +0300

    reorganize files

--- lena_moammar/urls.py
+++ lena_moammar/urls.py
@@ -14,11 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from . import views
 
-app_name = "faye_alsharikh"
+app_name = "lena_moammar"
 urlpatterns = [
     path("", views.home, name="home"),
 ]
